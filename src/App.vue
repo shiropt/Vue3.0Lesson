83a76242303c252a6d5ba956beb02cd6be463baf
shiropt <63380875+shiropt@users.noreply.github.com>
2020-10-27 18:57:03 +0900

    import Button Components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
     <h1>HelloWorld.</h1>
+    <Button />
   </div>
 </template>
 
 <script>
+import Button from "@/components/Button.vue"
 
 export default {
   name: 'App',
   components: {
+    Button
   }
 }
 </script>
